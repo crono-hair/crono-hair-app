5060fe8768d5f1c7ced26596220e39371280b34a
Vitor <vitoraugusto002@hotmail.com>
2022-06-20 22:36:02 -0300

    add Spacer doc

--- src/components/Spacer/Spacer.tsx
+++ src/components/Spacer/Spacer.tsx
@@ -19,6 +19,22 @@ interface ISpacer {
   mx?: number;
 }
 
+/**
+ * Component for spacing (margin and padding)\
+ * "m" refers to margin\
+ * "p" refers to padding
+ * ____________________________________________
+ *
+ * Use "t", "r", "b", "l", "y" or "x" to specify the spacing direction\
+ * **First example**: same of `margin-top: 12px`\
+ * **Second example**: same of `padding: 8px 0px`\
+ * **Third example**: same of `padding: 10px`
+ *
+ * @example <Spacer mt={12} />
+ * @example <Spacer py={8} />
+ * @example <Spacer p={10} />
+ */
+
 const Spacer: React.FC<ISpacer> = ({
   p,
   pt,
