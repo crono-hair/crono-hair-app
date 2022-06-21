d871b4949665853d83381c276789509514f4f462
Vitor <vitoraugusto002@hotmail.com>
2022-06-21 00:06:47 -0300

    create Text component

---
+++ src/components/Typography/Text.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+interface IText {
+  size?: number;
+  color?: string;
+  align?: string;
+  weight?: 'normal' | 'bold';
+  lineHeight?: number;
+  spacing?: number;
+  style?: object;
+  children: React.ReactChild;
+}
+
+/**
+ * Renders a text
+ *
+ * @example
+ * <Text color="lightgray" weight="bold" spacing={0.8}>Hello World</Text>
+ */
+
+const Text: React.FC<IText> = ({
+  size = 16,
+  color,
+  align,
+  weight,
+  lineHeight,
+  spacing,
+  style,
+  children,
+}) => {
+  return (
+    <StyledText
+      weight={weight}
+      color={color}
+      align={align}
+      size={size}
+      lineHeight={lineHeight}
+      spacing={spacing}
+      style={{ ...style }}>
+      {children}
+    </StyledText>
+  );
+};
+
+const StyledText = styled.Text<IText>`
+  font-weight: ${props => (props.weight ? props.weight : 'normal')};
+  font-size: ${props => (props.size ? props.size + 'px' : '16px')};
+  color: ${props => (props.color ? props.color : '#fff')};
+  text-align: ${props => (props.align ? props.align : 'left')};
+  ${props => (props.lineHeight ? `line-height:${props.lineHeight}px` : null)};
+  ${props => (props.spacing ? `letter-spacing:${props.spacing}px` : null)};
+`;
+
+export default Text;
