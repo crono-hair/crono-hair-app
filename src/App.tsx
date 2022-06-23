c4773f24fc14544805572d149eeeafb8af31c127
Vitor <vitoraugusto002@hotmail.com>
2022-06-22 23:59:58 -0300

    change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SafeArea, Spacer, Text } from './components';
 
-import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import Button from './components/UI/Button';
+import Input from './components/UI/Input';
 
 const App = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <Text>Crono Hair App</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <SafeArea>
+      <Spacer p={16}>
+        <Input
+          label="Email"
+          onChangeText={setUsername}
+          value={username}
+          placeholder="cronohair@exemplo.com"
+        />
+        <Spacer mt={8} />
+        <Input
+          label="Senha"
+          onChangeText={setPassword}
+          value={password}
+          placeholder="********"
+          secureTextEntry
+        />
+        <Spacer mt={16} />
+        <Button text="Acessar aplicativo" />
+      </Spacer>
+    </SafeArea>
   );
 };
 
