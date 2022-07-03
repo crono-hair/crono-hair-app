62ae3229124792d2b0cac4e9ebe276867866d19a
Vitor <vitoraugusto002@hotmail.com>
2022-07-03 01:41:36 -0300

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React, { useState } from 'react';
-import { SafeArea, Spacer, Text } from './components';
+import { SafeArea, Spacer } from './components';
 
 import Button from './components/UI/Button';
 import Input from './components/UI/Input';
@@ -9,26 +10,28 @@ const App = () => {
   const [password, setPassword] = useState<string>('');
 
   return (
-    <SafeArea>
-      <Spacer p={16}>
-        <Input
-          label="Email"
-          onChangeText={setUsername}
-          value={username}
-          placeholder="cronohair@exemplo.com"
-        />
-        <Spacer mt={8} />
-        <Input
-          label="Senha"
-          onChangeText={setPassword}
-          value={password}
-          placeholder="********"
-          secureTextEntry
-        />
-        <Spacer mt={16} />
-        <Button text="Acessar aplicativo" />
-      </Spacer>
-    </SafeArea>
+    <NavigationContainer>
+      <SafeArea>
+        <Spacer p={16}>
+          <Input
+            label="Email"
+            onChangeText={setUsername}
+            value={username}
+            placeholder="cronohair@exemplo.com"
+          />
+          <Spacer mt={8} />
+          <Input
+            label="Senha"
+            onChangeText={setPassword}
+            value={password}
+            placeholder="********"
+            secureTextEntry
+          />
+          <Spacer mt={16} />
+          <Button text="Acessar aplicativo" />
+        </Spacer>
+      </SafeArea>
+    </NavigationContainer>
   );
 };
 
