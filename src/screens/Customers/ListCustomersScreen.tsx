b0f93254c1e3c294ca8ce38ad4f38ffc626273f3
Vitor <vitoraugusto002@hotmail.com>
2022-07-05 23:57:39 -0300

    create ListCustomersScreen

---
+++ src/screens/Customers/ListCustomersScreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../navigation';
+
+import { SafeArea, Spacer, Text } from '../../components';
+
+const ListCustomers: React.FC<
+  NativeStackScreenProps<RootStackParamList, 'ListCustomers'>
+> = ({ route }) => {
+  const { name, sex, birthdate, phone } = route.params || {};
+
+  return (
+    <SafeArea>
+      <Spacer p={16}>
+        <Text size={20}>Nome: {name}</Text>
+        <Text size={20}>Sexo: {sex}</Text>
+        <Text size={20}>Data de nascimento: {birthdate}</Text>
+        <Text size={20}>Número para contato: {phone}</Text>
+      </Spacer>
+    </SafeArea>
+  );
+};
+
+export default ListCustomers;
