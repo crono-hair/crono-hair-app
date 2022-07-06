dd1ef264ee827b816e52c2946f51d417c972c5c9
Vitor <vitoraugusto002@hotmail.com>
2022-07-05 23:54:22 -0300

    create LoginScreen

---
+++ src/screens/Login/LoginScreen.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+
+import { Button, Input, SafeArea, Spacer } from '../../components';
+
+const LoginScreen: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  return (
+    <SafeArea>
+      <Spacer mt={200} />
+      <Spacer p={16}>
+        <Input
+          label="Email"
+          onChangeText={setUsername}
+          value={username}
+          placeholder="cronohair@exemplo.com"
+        />
+        <Spacer mt={8} />
+        <Input
+          label="Senha"
+          onChangeText={setPassword}
+          value={password}
+          placeholder="********"
+          secureTextEntry
+        />
+        <Spacer mt={16} />
+        <Button text="Acessar aplicativo" />
+      </Spacer>
+    </SafeArea>
+  );
+};
+
+export default LoginScreen;
