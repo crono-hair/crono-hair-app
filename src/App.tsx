7d97f1c40e18dadb7de15eb3e1371f37145d922b
Vitor <vitoraugusto002@hotmail.com>
2022-07-05 23:58:37 -0300

    add Routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React, { useState } from 'react';
-import { SafeArea, Spacer } from './components';
-
-import Button from './components/UI/Button';
-import Input from './components/UI/Input';
+import React from 'react';
+import Routes from './navigation';
 
 const App = () => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-
-  return (
-    <NavigationContainer>
-      <SafeArea>
-        <Spacer p={16}>
-          <Input
-            label="Email"
-            onChangeText={setUsername}
-            value={username}
-            placeholder="cronohair@exemplo.com"
-          />
-          <Spacer mt={8} />
-          <Input
-            label="Senha"
-            onChangeText={setPassword}
-            value={password}
-            placeholder="********"
-            secureTextEntry
-          />
-          <Spacer mt={16} />
-          <Button text="Acessar aplicativo" />
-        </Spacer>
-      </SafeArea>
-    </NavigationContainer>
-  );
+  return <Routes />;
 };
 
 export default App;
