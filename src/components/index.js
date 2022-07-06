57ad75fbd0e938d31b4ea43abda760b2656b806d
Vitor <vitoraugusto002@hotmail.com>
2022-07-05 23:53:20 -0300

    add Button and Input in components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import SafeArea from './Layout/SafeArea';
 import Spacer from './Layout/Spacer';
 import Text from './Typography/Text';
+import Button from './UI/Button';
+import Input from './UI/Input';
 
-export { SafeArea, Spacer, Text };
+export { SafeArea, Spacer, Text, Button, Input };
