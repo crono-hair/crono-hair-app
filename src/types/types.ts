00a060a092c08c1015630ad32cad06f0a1782c4d
Vitor <vitoraugusto002@hotmail.com>
2022-07-06 21:16:14 -0300

    fix types

---
+++ src/types/types.ts
@@ -0,0 +1,72 @@
+import {
+  NativeStackNavigationOptions,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack';
+
+import { TextInputProps, TouchableOpacityProps } from 'react-native';
+
+export type RootStackParamList = {
+  Login: undefined;
+  CreateCustomer: undefined;
+  ListCustomers: {
+    name: string;
+    birthdate: string;
+    sex: string;
+    phone: string;
+  };
+};
+
+export interface INavigation {
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+}
+
+export interface IRoute {
+  name: keyof RootStackParamList;
+  options: NativeStackNavigationOptions;
+  component: React.FC<any>;
+}
+
+export interface ISafeArea {
+  statusBarBgColor?: string;
+  barStyle?: 'light' | 'dark';
+  bgColor?: string;
+  children: React.ReactChild | React.ReactChild[];
+}
+
+export interface IInput extends TextInputProps {
+  placeholder: string;
+  label?: string;
+}
+
+export interface ISpacer {
+  p?: number;
+  pt?: number;
+  pr?: number;
+  pb?: number;
+  pl?: number;
+  py?: number;
+  px?: number;
+  m?: number;
+  mt?: number;
+  mr?: number;
+  mb?: number;
+  ml?: number;
+  my?: number;
+  mx?: number;
+  children?: React.ReactChild | React.ReactChild[];
+}
+
+export interface IText {
+  size?: number;
+  color?: string;
+  align?: string;
+  weight?: 'normal' | 'bold';
+  lineHeight?: number;
+  spacing?: number;
+  style?: object;
+  children?: React.ReactChild | React.ReactChild[];
+}
+
+export interface IButton extends TouchableOpacityProps {
+  text: string;
+}
